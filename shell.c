9ee0924d50c5747a5d445e073a589f669a9740b7
Eric Wardner <ewardner22@gmail.com>
2015-02-06 08:43:39 -0700

    early commit

--- shell.c
+++ shell.c
@@ -1,11 +1,15 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include <string.h>
 #include <unistd.h>
+#include "list.h"
 
 int main(){
     //intialize input buffer
     char input[64];
+    char* cmd;
     char cwd[512];
+    node* history = NULL;
 /*
     printf("-------------------------------------------\n");
     printf("-------------------------------------------\n");
@@ -15,11 +19,15 @@ int main(){
 */
     //while loop to poll for input
     while(strcmp(input, "exit") != 0){
-        printf("\n");
+        //gets current directory and prints it out
         getcwd(cwd, sizeof(cwd));
         printf ("%s:8==D ", cwd);
         fflush (stdout);
-        fgets(input, 80, stdin);
+        //gets user input and tokenizes it
+        fgets(input, 64, stdin);
+        cmd = strtok(input, "\n");
+        history = list_insert_tail(history, cmd);
+        list_print(history);
         system(input);
     }
     return 0;
