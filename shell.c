2308df8eb6b67b90877675cebea5234f9a267e9a
Eric Wardner <ewardner22@gmail.com>
2015-02-08 16:33:59 -0700

    history command might be done

--- shell.c
+++ shell.c
@@ -8,11 +8,13 @@
 node* history = NULL;
 void printHistory(int);
 void insertHistory(char*);
+void strToArray(char*, char**);
 
 int main(){
     //intialize input buffer
     char input[64];
     char* cmd;
+    char* cmdArray[16];
     char cwd[512];
     
     //while loop to poll for input
@@ -21,19 +23,25 @@ int main(){
         getcwd(cwd, sizeof(cwd));
         printf ("%s:8==D ", cwd);
         fflush (stdout);
+
         //gets user input and tokenizes it
         fgets(input, 64, stdin);
         cmd = strtok(input, "\n");
         insertHistory(cmd);
-        if(strcmp(input, "history") == 0){
-            int num = 2;
+        strToArray(cmd, cmdArray);
+
+        //checks what the command was
+        int num = 10;
+        if(strcmp(cmdArray[0], "history") == 0){
+            if(cmdArray[1] != NULL){
+                //printf("\n\n%d\n\n", atoi(cmdArray[1]));
+                num = atoi(cmdArray[1]);
+            }
             printHistory(num);
         }else{
             system(input);
         }
     }
-    
-    return 0;
 }
 
 void insertHistory(char* cmd){
@@ -41,19 +49,33 @@ void insertHistory(char* cmd){
     history = list_remove(history, cmd);
     //insert new cmd
     history = list_insert_tail(history, cmd);
-    return;
 }
 
 void printHistory(int numCalls){
     int i;
     int size = list_length(history);
     char* cmd;
-
-    for(i = 0; i < size; i++){
+    
+    for(i = 1; i <= size; i++){
         if(i > size - numCalls){
             cmd = list_get(history, i);
             printf("%d: %s\n", i, cmd);
         }
-    }
+    } 
+}
+
+void strToArray(char* string, char* cmdArray[]){
+    int i = 0;
+    char* token = strtok(string, " ");
     
-}            
+    while(token != NULL){
+        cmdArray[i] = malloc(strlen(token) + 1);
+        strcpy(cmdArray[i], token);
+        token = strtok(NULL, " ");
+        i++;
+    }
+    cmdArray[i] = NULL;
+}       
+
+
+
